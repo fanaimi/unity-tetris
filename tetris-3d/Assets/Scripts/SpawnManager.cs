f7941f7f7342c857200ad85d43f495709724b104
fanaimi <emiliano.fantasia@gmail.com>
2021-12-01 11:38:47 +0000

    completed preview, need to rearrange preview osition

--- tetris-3d/Assets/Scripts/SpawnManager.cs
+++ tetris-3d/Assets/Scripts/SpawnManager.cs
@@ -6,11 +6,13 @@ public class SpawnManager : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] m_allTetramins;
+    [SerializeField] private GameObject[] m_allStaticTetramins;
 
-    private GameObject m_firstPiece;
+    private int m_nextPieceIndex;
     private GameObject m_nextPiece;
+    private GameObject m_nextPiecePreview;
     
-    private Vector3 m_nextPiecePosition = new Vector3(10, 25, 0);
+    private Vector3 m_previewPosition = new Vector3(9.5f, 22.5f, 0.2f);
     
     // Start is called before the first frame update
     void Start()
@@ -27,29 +29,29 @@ public class SpawnManager : MonoBehaviour
 
     private void GetNextPiece()
     {
-        if (m_nextPiece != null)
+        if (m_nextPiecePreview != null)
         {
-            Destroy(m_nextPiece);
-            m_nextPiece = null;
+            Destroy(m_nextPiecePreview.gameObject);
+            m_nextPiecePreview = null;
         }
 
-        int m_randomIndex = Random.Range(0, m_allTetramins.Length);
-        m_nextPiece= m_allTetramins[m_randomIndex];
+        m_nextPieceIndex = Random.Range(0, m_allTetramins.Length);
+        m_nextPiece = m_allTetramins[m_nextPieceIndex];
+        m_nextPiecePreview = m_allStaticTetramins[m_nextPieceIndex];
         PreviewNextPiece();
     } // GetNextPiece
 
     public void SpawnPiece()
     {
-        Instantiate(m_nextPiece, transform.position, transform.rotation);
+        Instantiate(m_allTetramins[m_nextPieceIndex], transform.position, transform.rotation);
+        GetNextPiece();
     } //  SpawnPiece
     
     
 
     private void PreviewNextPiece()
     {
-        
-        Instantiate(m_nextPiece, m_nextPiecePosition, transform.rotation);
-        m_nextPiece.isStatic = true;
+        m_nextPiecePreview = Instantiate(m_allStaticTetramins[m_nextPieceIndex], m_previewPosition, transform.rotation);
     } // m_nextPiece
 
 
